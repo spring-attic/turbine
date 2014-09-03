5fd0e2e8b89e5dacb54a334dd58b2a8b147293ff
Spencer Gibb <spencer@gibb.us>
2014-09-03 12:39:57 -0600

    updated to use @EnableTurbine

--- src/main/java/turbine/TurbineApplication.java
+++ src/main/java/turbine/TurbineApplication.java
@@ -1,86 +1,19 @@
 package turbine;
 
-import com.netflix.turbine.discovery.InstanceDiscovery;
-import com.netflix.turbine.init.TurbineInit;
-import com.netflix.turbine.plugins.PluginsFactory;
-import com.netflix.turbine.streaming.servlet.TurbineStreamServlet;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.context.embedded.ServletRegistrationBean;
-import org.springframework.boot.context.web.SpringBootServletInitializer;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.context.SmartLifecycle;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
+import org.springframework.cloud.netflix.turbine.EnableTurbine;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.Ordered;
 
 /**
  * Created by sgibb on 7/11/14.
  */
 @Configuration
-@ComponentScan
 @EnableAutoConfiguration
-@EnableEurekaClient
-public class TurbineApplication extends SpringBootServletInitializer implements SmartLifecycle, Ordered {
-
-    @Override
-    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-        return application.sources(TurbineApplication.class).web(true);
-    }
+@EnableTurbine
+public class TurbineApplication {
 
     public static void main(String[] args) {
         new SpringApplicationBuilder(TurbineApplication.class).web(true).run(args);
-    }
-
-    @Bean
-    public ServletRegistrationBean turbineStreamServlet() {
-        return new ServletRegistrationBean(new TurbineStreamServlet(), "/turbine.stream");
-    }
-
-    @Bean
-    public InstanceDiscovery instanceDiscovery() {
-        return new EurekaInstanceDiscovery();
-    }
-
-    private boolean running;
-
-    @Override
-    public boolean isAutoStartup() {
-        return true;
-    }
-
-    @Override
-    public void stop(Runnable callback) {
-        callback.run();
-    }
-
-    @Override
-    public void start() {
-        //TODO: figure out ordering, so this is already run by EurekaClientConfiguration
-        //DiscoveryManager.getInstance().initComponent(instanceConfig, clientConfig);
-        PluginsFactory.setClusterMonitorFactory(new SpringAggregatorFactory());
-        PluginsFactory.setInstanceDiscovery(instanceDiscovery());
-        TurbineInit.init();
-    }
-
-    @Override
-    public void stop() {
-        running = false;
-    }
-
-    @Override
-    public boolean isRunning() {
-        return running;
-    }
-
-    @Override
-    public int getPhase() {
-        return 0;
-    }
-
-    @Override
-    public int getOrder() {
-        return -1;
     }
 }
