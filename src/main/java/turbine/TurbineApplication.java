08a888902ba83dea9b54f2b8da8c580f89585a92
Spencer Gibb <spencer@gibb.us>
2014-12-17 14:55:08 -0700

    updated to use new @EnableTurbineAmqp

--- src/main/java/turbine/TurbineApplication.java
+++ src/main/java/turbine/TurbineApplication.java
@@ -2,8 +2,9 @@ package turbine;
 
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.cloud.bus.turbine.EnableBusTurbine;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.netflix.turbine.amqp.EnableTurbineAmqp;
 
 /**
  * @author Spencer Gibb
@@ -11,10 +12,11 @@ import org.springframework.context.annotation.Configuration;
  */
 @Configuration
 @EnableAutoConfiguration
-@EnableBusTurbine
+@EnableTurbineAmqp
+@EnableDiscoveryClient
 public class TurbineApplication {
 
     public static void main(String[] args) {
-        new SpringApplicationBuilder(TurbineApplication.class).web(false).run(args);
+        new SpringApplicationBuilder(TurbineApplication.class).run(args);
     }
 }
