1a9375527c27d951ee008f93aca6106051e74a47
Dave Syer <dsyer@pivotal.io>
2014-12-17 11:34:54 +0000

    Purge starters from higher up the stack

--- src/main/java/turbine/TurbineApplication.java
+++ src/main/java/turbine/TurbineApplication.java
@@ -2,9 +2,8 @@ package turbine;
 
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.cloud.bus.turbine.StartBusTurbine;
+import org.springframework.cloud.bus.turbine.EnableBusTurbine;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Import;
 
 /**
  * @author Spencer Gibb
@@ -12,7 +11,7 @@ import org.springframework.context.annotation.Import;
  */
 @Configuration
 @EnableAutoConfiguration
-@Import(StartBusTurbine.class)
+@EnableBusTurbine
 public class TurbineApplication {
 
     public static void main(String[] args) {
