f44aaf6ca5d5d1367c9bd902d1ddc75b0f00acc2
Dave Syer <dsyer@pivotal.io>
2015-01-02 15:19:59 +0000

    Switch off eureka logging

--- src/main/java/turbine/TurbineApplication.java
+++ src/main/java/turbine/TurbineApplication.java
@@ -3,8 +3,8 @@ package turbine;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.turbine.amqp.EnableTurbineAmqp;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.netflix.turbine.amqp.EnableTurbineAmqp;
 
 /**
  * @author Spencer Gibb
