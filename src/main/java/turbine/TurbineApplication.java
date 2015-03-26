6f3d3bd978bf1186572181b303ed23cd3d66f602
Dave Syer <dsyer@pivotal.io>
2015-03-26 13:57:33 +0000

    Switch off web environment in cloud profile

--- src/main/java/turbine/TurbineApplication.java
+++ src/main/java/turbine/TurbineApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.turbine.amqp.EnableTurbineAmqp;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.StandardEnvironment;
 
 /**
  * @author Spencer Gibb
@@ -16,7 +17,8 @@ import org.springframework.context.annotation.Configuration;
 @EnableDiscoveryClient
 public class TurbineApplication {
 
-    public static void main(String[] args) {
-        new SpringApplicationBuilder(TurbineApplication.class).run(args);
-    }
+	public static void main(String[] args) {
+		boolean cloudEnvironment = new StandardEnvironment().acceptsProfiles("cloud");
+		new SpringApplicationBuilder(TurbineApplication.class).web(!cloudEnvironment).run(args);
+	}
 }
